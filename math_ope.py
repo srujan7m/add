995f5b5459258dcc5be0f2c35db7dc2a583399d3
srujan7m <srujanmmattur@gmail.com>
2025-10-28 12:01:51 +0530

    operations

---
+++ math_ope.py
@@ -0,0 +1,9 @@
+a=20
+b=10
+print("The addition is:", a + b)
+print("The subtraction is:", a - b)
+print("The multiplication is:", a * b)
+print("The division is:", a / b)
+print("The square of a and b is:", a ** 2, b ** 2)
+print("The cube of a and b is:", a ** 3, b ** 3)
+print("The power of a and b is:",pow(a,b))
